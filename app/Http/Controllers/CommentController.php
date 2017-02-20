fba56474fa9ca4c8247c7ac6b5897781bfe01781
evgenyapanda <funeralpanda@mail.ru>
2017-02-20 15:43:48 +0200

    adding comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -17,6 +17,7 @@ use Validator;
 
 
 
+
 class CommentController extends SiteController
 {
     /**
@@ -84,7 +85,17 @@ class CommentController extends SiteController
 
         $post->comments()->save($comment);
 
-        echo json_encode(['hello' => 'world']);
+        $comment->load('user');
+        $data['id'] = $comment->id;
+
+        $data['email'] = (!empty($data['email'])) ? $data['email'] : $comment->user->email;
+        $data['name'] = (!empty($data['name'])) ? $data['name'] : $comment->user->name;
+
+        $data['hash'] = md5($data['email']);
+
+        $view_comment = view(env('THEME').'.content_one_comment')->with('data',$data)->render();
+
+        return \Response::json(['success' => true, 'comment' => $view_comment, 'data' => $data]);
         exit();
     }
 
