986286b2020e29e1fef95f82315c7661c61a1c61
evgenyapanda <funeralpanda@mail.ru>
2017-02-24 14:57:53 +0200

    portfolios

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -3,8 +3,45 @@
 namespace Corp\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Corp\Repositories\PortfoliosRepository;
 
-class PortfolioController extends Controller
+class PortfolioController extends SiteController
 {
     //
+    public function __construct(PortfoliosRepository $p_rep)
+    {
+
+        parent::__construct(new \Corp\Repositories\MenusRepository((new \Corp\Menu)));
+
+        $this->p_rep = $p_rep;
+
+        $this->template = env('THEME') . '.portfolios';
+    }
+
+
+    public function index()
+    {
+        //
+
+        $this->title = 'Портфолио';
+        $this->keywords = 'Портфолио';
+        $this->meta_desc = 'Портфолио';
+
+        $portfolios = $this->getPortfolios();
+
+        $content = view(env('THEME').'.portfolios_content')->with('portfolios', $portfolios)->render();
+        $this->vars = array_add($this->vars, 'content', $content);
+
+
+        return $this->renderOutput();
+    }
+
+    public function getPortfolios(){
+        $portfolios = $this->p_rep->get('*', false, true);
+        if($portfolios){
+            $portfolios->load('filter');
+        }
+
+        return $portfolios;
+    }
 }
